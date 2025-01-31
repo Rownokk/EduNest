2f88670a78d4c7cdc6e849f5438e07733c16c1e4
jamila143 <jamila.cse.20220104143@aust.edu>
2025-01-31 23:56:32 +0600

    popular course contact us

--- index.php
+++ index.php
@@ -78,11 +78,145 @@
 
     </div>
  <!-- end Banner --> 
+<!--start most popular course-->
+
+ <div class="container mt-5">
+  <h1 class ="text-center">Popular Course</h1>
+ <!-- Start Most Popular Course 1st Card Deck -->
+<div class="container mt-4">
+  <div class="d-flex flex-wrap justify-content-center gap-3">
+    
+    <div class="card" style="width:25rem;">
+      <img src="images/download (4).jpeg" class="card-img-top" alt="Guitar">
+      <div class="card-body">
+        <h5 class="card-title">Learn Guitar Easy Way</h5>
+        <p class="card-text">Lorem ipsum dolor sit meet consectetur adipisicing elit. Facilitis, nemo.</p>
+      </div>
+      <div class="card-footer d-flex justify-content-between align-items-center">
+        <p class="card-text mb-0">Price: <small><del>&#8377 2000</del></small> <span class="font-weight-bolder">&#8377 200</span></p>
+        <a class="btn btn-primary text-white font-weight-bolder" href="#">Enroll</a>
+      </div>
+    </div>
+
+    <div class="card" style="width:25rem;">
+      <img src="images/download (4).jpeg" class="card-img-top" alt="Guitar">
+      <div class="card-body">
+        <h5 class="card-title">Learn Guitar Easy Way</h5>
+        <p class="card-text">Lorem ipsum dolor sit meet consectetur adipisicing elit. Facilitis, nemo.</p>
+      </div>
+      <div class="card-footer d-flex justify-content-between align-items-center">
+        <p class="card-text mb-0">Price: <small><del>&#8377 2000</del></small> <span class="font-weight-bolder">&#8377 200</span></p>
+        <a class="btn btn-primary text-white font-weight-bolder" href="#">Enroll</a>
+      </div>
+    </div>
+
+    <div class="card" style="width:25rem;">
+      <img src="images/download (4).jpeg" class="card-img-top" alt="Guitar">
+      <div class="card-body">
+        <h5 class="card-title">Learn Guitar Easy Way</h5>
+        <p class="card-text">Lorem ipsum dolor sit meet consectetur adipisicing elit. Facilitis, nemo.</p>
+      </div>
+      <div class="card-footer d-flex justify-content-between align-items-center">
+        <p class="card-text mb-0">Price: <small><del>&#8377 2000</del></small> <span class="font-weight-bolder">&#8377 200</span></p>
+        <a class="btn btn-primary text-white font-weight-bolder" href="#">Enroll</a>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
+<!-- End Most Popular Course 1st Card Deck --> 
+<!-- Start Most Popular Course 2nd Card Deck -->
+<div class="container mt-4">
+  <div class="d-flex flex-wrap justify-content-center gap-3">
+    <div class="card" style="width: 25rem;">
+      <img src="images/download (3).jpeg" class="card-img-top" alt="Python">
+      <div class="card-body">
+        <h5 class="card-title">Learn Python</h5>
+        <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat, error.</p>
+      </div>
+      <div class="card-footer d-flex justify-content-between align-items-center">
+        <p class="card-text mb-0">Price: <small><del>&#8377 3000</del></small> <span class="font-weight-bolder">&#8377 300</span></p>
+        <a class="btn btn-primary text-white font-weight-bolder" href="#">Enroll</a>
+      </div>
+    </div>
+
+    <div class="card" style="width: 25rem;">
+      <img src="images/download (3).jpeg" class="card-img-top" alt="Python">
+      <div class="card-body">
+        <h5 class="card-title">Learn Python</h5>
+        <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat, error.</p>
+      </div>
+      <div class="card-footer d-flex justify-content-between align-items-center">
+        <p class="card-text mb-0">Price: <small><del>&#8377 3000</del></small> <span class="font-weight-bolder">&#8377 300</span></p>
+        <a class="btn btn-primary text-white font-weight-bolder" href="#">Enroll</a>
+      </div>
+    </div>
+
+    <div class="card" style="width: 25rem;">
+      <img src="images/download (3).jpeg" class="card-img-top" alt="Python">
+      <div class="card-body">
+        <h5 class="card-title">Learn Python</h5>
+        <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat, error.</p>
+      </div>
+      <div class="card-footer d-flex justify-content-between align-items-center">
+        <p class="card-text mb-0">Price: <small><del>&#8377 3000</del></small> <span class="font-weight-bolder">&#8377 300</span></p>
+        <a class="btn btn-primary text-white font-weight-bolder" href="#">Enroll</a>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- End Most Popular Course 2nd Card Deck -->
+
+<div class="text-center m-2">
+  <a class="btn btn-danger btn-sm" href="#">View All Course</a>
+</div>
+
+
+<!--End most popular course-->
+<!-- Start Contact Us --> 
+<div class="container mt-4" id="Contact"> <!--Start Contact 
+Us Container--> 
+<h2 class="text-center mb-4">Contact US</h2> <!-- Contact Us 
+Heading --> 
+<div class="row"> <!--Start Contact Us Row--> 
+<div class="col-md-8"> <!--Start Contact Us 1st Column--> 
+<form action="" method="post"> 
+<input type="text" class="form-control" name="name" 
+placeholder="Name"><br> 
+<input type="text" class="form-control" name="subject" 
+placeholder="Subject"><br> 
+<input type="email" class="form-control" name="email" 
+placeholder="E-mail"><br> 
+<textarea class="form-control" name="message" placeholder="How can we help you?" style="height:150px; 
+"></textarea><br> 
+<input class="btn btn-primary" type="submit" 
+value="Send" name="submit"><br><br> 
+</form> 
+</div> <!-- End Contact Us 1st Column--> 
+<div class="col-md-4 stripe text-white text-center"> <!--
+Start Contact Us 2nd Column--> 
+<h4>iSchool</h4> 
+<p>iSchool, 
+Near Police Camp II, Bokaro, 
+Jharkhand 834005<br /> 
+Phone: +00000000 <br /> 
+www.ischool.com </p> 
+</div> <!-- End Contact Us 2nd Column--> 
+</div> <!-- End Contact Us Row--> 
+
+</div> <!-- End Contact Us Container--> 
+
+<!-- End Contact Us --> 
+
+
 
     <!-- Bootstrap & Jquery JS -->
   <script src="js/jquery.min.js"></script>  
   <script src="js/popper.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
+
   <!-- Font Awesome JS -->
   <script src="js/all.min.js"></script>
 </body>
