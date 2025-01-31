ff06d559ec513bca2882efb74972b2b95bd8d964
Rownok Jahan Mowmita <151154578+Rownokk@users.noreply.github.com>
2025-01-31 12:55:55 +0600

    setup

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap Css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="css/all.min.css">
+    <title>EduNest</title>
+    
+</head>
+<body>
+    <h1>Home Page</h1>
+    <!-- Bootstrap & Jquery JS -->
+  <script src="js/jquery.min.js"></script>  
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <!-- Font Awesome JS -->
+  <script src="js/all.min.js"></script>
+</body>
+</html>
