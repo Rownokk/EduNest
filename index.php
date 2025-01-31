0dd7bfa5005c7a7d7a121df4ea53e909794881f4
Rownok Jahan Mowmita <151154578+Rownokk@users.noreply.github.com>
2025-01-31 14:59:30 +0600

    navbae css

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- google font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&family=Outfit:wght@100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="css/style.css">
     <!-- Bootstrap Css -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <!-- Font Awesome CSS -->
@@ -12,24 +17,24 @@
 </head>
 <body>
      <!-- Start Navigation -->
-     <nav class="navbar navbar-expand-sm navbar-light bg-light">
+     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="index.php">EduNest</a>
-    <span class="navbar-test">Learn & Implement</span>
+    <span class="navbar-text">Learn & Implement</span>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <ul class="navbar-nav">
-        <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Course</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Payment Status</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">My Profile</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Logout</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">FeedBack</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+      <ul class="navbar-nav custom-nav">
+        <li class="nav-item custom-nav-item"><a href="index.php" class="nav-link">Home</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Course</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Payment Status</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">My Profile</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Logout</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Login</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Signup</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">FeedBack</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Contact</a></li>
       </ul>
     </div>
   </div>
