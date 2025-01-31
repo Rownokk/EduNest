baf3ba180cddcc77b6f5dfbc24833d92f1717c8f
Rownok Jahan Mowmita <151154578+Rownokk@users.noreply.github.com>
2025-01-31 13:44:08 +0600

    Navbar

--- index.php
+++ index.php
@@ -11,7 +11,30 @@
     
 </head>
 <body>
-    <h1>Home Page</h1>
+     <!-- Start Navigation -->
+     <nav class="navbar navbar-expand-sm navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">EduNest</a>
+    <span class="navbar-test">Learn & Implement</span>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <ul class="navbar-nav">
+        <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Course</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Payment Status</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">My Profile</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Logout</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">FeedBack</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+      <!-- End Navigation -->
     <!-- Bootstrap & Jquery JS -->
   <script src="js/jquery.min.js"></script>  
   <script src="js/popper.min.js"></script>
