ac069fe6b3fcb2148a189bc442f42eeee63d4c81
Authoi Chowdhury <authoichy016@gmail.com>
2025-02-06 00:46:04 +0600

    sign up

--- index.php
+++ index.php
@@ -404,8 +404,9 @@ www.edunest.com </p>
 
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-primary" >Sign up</button>
+        <button type="button" class="btn btn-secondary"
+        data-dismiss="modal">Close</button>
       </div>
     </div>
   </div>
